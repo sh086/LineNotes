77a6e933ad8cc434f7689795c71ce1991f8e66e8
suhe <suheforvip@gmail.com>
2021-11-16 01:38:46 +0800

    add:增加docker笔记

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -1,6 +1,8 @@
 module.exports = [
     {text: 'Blog', link: '/blog/'},
     {text: 'Guide', link: '/guide/'},
+    // {text: 'Data', link: '/data/'},
+    // {text: 'Smart', link: ''},
     {text: 'Quantity', link: '/quantity/'},
     {text: 'Media', link: '/media/'},
     {text: 'Project', link: '/project/'},
